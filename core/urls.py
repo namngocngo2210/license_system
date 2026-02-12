16f3f546992aec631ca02f911ceb02bdb500dbba
Ngo Ngoc Nam <namngocngo2210@gmail.com>
2026-02-12 13:42:41 +0700

    update api

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import LogoutView
+from .views import LogoutView, LoginView
 
 urlpatterns = [
-    path('login/', obtain_auth_token, name='api_token_auth'),
+    path('login/', LoginView.as_view(), name='api_token_auth'),
     path('logout/', LogoutView.as_view(), name='api_token_logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,25 @@
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+from rest_framework.permissions import AllowAny
+
+class LoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+    authentication_classes = []
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        })
 
 class LogoutView(APIView):
     def post(self, request):
